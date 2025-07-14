1d0104cb3c03ea3ac46794a88f08959c3c020ade
iot-Noob <talhakhalid2018@gmail.com>
2025-07-14 18:16:51 +0500

    added langchain linrary

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-import os
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
+texts = ["LangChain is great!", "ChromaDB stores vectors locally."]
 
-for root,_,file in os.walk("./Roles/"):
-    for f in file:
-        print(f)
+embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+db = Chroma.from_texts(texts, embedding=embedding, persist_directory="./chroma_db")
+
+results = db.similarity_search("What is LangChain?", k=1)
+print(results[0].page_content)
