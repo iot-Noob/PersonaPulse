54bdf6aeb3459c2b3bf00001064a4562310a4842
iot-Noob <talhakhalid2018@gmail.com>
2025-08-10 21:33:05 -0700

    toast appear on top of everything

--- Frontend/talhagpt/src/main.jsx
+++ Frontend/talhagpt/src/main.jsx
@@ -5,8 +5,16 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.js";
-
+import { ToastContainer,toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+ 
 createRoot(document.getElementById("root")).render(
+<>
+<ToastContainer
+  position="top-right"
+  autoClose={7000}
+  style={{ zIndex: 999999 }}
+/>
   <Provider store={store}>
     <StrictMode>
       <BrowserRouter>
@@ -14,4 +22,5 @@ createRoot(document.getElementById("root")).render(
       </BrowserRouter>
     </StrictMode>
   </Provider>
+</>
 );
